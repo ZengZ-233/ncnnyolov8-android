09618592c9689324aaf258df343af1768e974ad4
ZengZ-233 <147756443+ZengZ-233@users.noreply.github.com>
2024-08-28 21:22:38 +0800

    Add files via upload

--- java/com/example/ncnn_yolo/MainActivity.java
+++ java/com/example/ncnn_yolo/MainActivity.java
@@ -1,53 +1,61 @@
 package com.example.ncnn_yolo;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
-
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ncnn_yolo.databinding.ActivityMainBinding;
+import com.example.ncnn_yolo.ui.home.HomeFragment;
+import com.example.ncnn_yolo.ui.theme.Theme;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private boolean isDarkTheme;
+    private static final String PREFS_NAME = "theme_prefs";
+    private static final String KEY_IS_DARK_THEME = "is_dark_theme";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//          theme
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        isDarkTheme = prefs.getBoolean(KEY_IS_DARK_THEME, false);
+
+        // Apply theme
+        Theme.applyTheme(this, isDarkTheme, true);
+
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
+        binding.appBarMain.fab.setOnClickListener(view ->
                 Snackbar.make(view, "bug联系1176017541@qq.com进行修改", Snackbar.LENGTH_LONG)
                         .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
+                        .setAnchorView(R.id.fab).show()
+        );
 
-            }
-
-        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_video, R.id.nav_img,R.id.nav_imgfix,R.id.nav_flv)
+                R.id.nav_home, R.id.nav_video, R.id.nav_img, R.id.nav_imgfix, R.id.nav_flv)
                 .setOpenableLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -55,9 +63,29 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_theme_switch) {
+            // Toggle theme
+            isDarkTheme = !isDarkTheme;
+
+            // Save theme preference
+            SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+            editor.putBoolean(KEY_IS_DARK_THEME, isDarkTheme);
+            editor.apply();
+
+            // Recreate activity to apply theme
+            recreate();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -66,6 +94,4 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-
-
 }
